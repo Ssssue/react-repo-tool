cd13469590cb76d17e734d5cfc50323222bb1917
苏堉 <suy@southzn.com>
2021-03-30 15:40:10 +0800

    Style: 暂时注释无用代码

--- react-repo-tool/src/Views/Main/index.tsx
+++ react-repo-tool/src/Views/Main/index.tsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Layout, Menu } from 'antd';
-import { MenuFoldOutlined } from '@ant-design/icons';
+import { Layout } from 'antd';
+// import { MenuFoldOutlined } from '@ant-design/icons';
 import { Route, Switch } from 'react-router-dom';
 import history from 'src/history';
 import { mainRoutes } from 'src/Router/index';
 import styles from './index.less';
 
-const { Header, Content, Sider } = Layout;
-const { SubMenu } = Menu;
+const { Content } = Layout;
+// const { SubMenu } = Menu;
 
 const Components = (): JSX.Element => {
-  const [menuKey, setMenuKey] = useState([mainRoutes[0].path]);
+  const [menuKey] = useState([mainRoutes[0].path]);
 
-  const [collapsed, setCollapsed] = useState(false);
+  // const [collapsed, setCollapsed] = useState(false);
 
   useEffect(() => {
     console.log('menuKey[0]: ', menuKey[0]);
@@ -21,47 +21,47 @@ const Components = (): JSX.Element => {
   }, []);
 
   //点击菜单：设置路由和选中菜单
-  const selectMenu = item => {
-    setMenuKey(item.selectedKeys);
-    history.push(item.selectedKeys[0]);
-  };
+  // const selectMenu = item => {
+  //   setMenuKey(item.selectedKeys);
+  //   history.push(item.selectedKeys[0]);
+  // };
 
   //点击收起或展开图标
-  const toggle = () => {
-    setCollapsed(!collapsed);
-  };
+  // const toggle = () => {
+  //   setCollapsed(!collapsed);
+  // };
   //菜单渲染
-  const MenuItem = menu => {
-    return menu.map(item => {
-      if (item.children && item.children.length > 0) {
-        return (
-          <SubMenu
-            key={item.path}
-            title={
-              <span>
-                <item.icon />
-                <span>{item.title}</span>
-              </span>
-            }
-          >
-            {MenuItem(item.children)}
-          </SubMenu>
-        );
-      }
-      return (
-        <Menu.Item key={item.path}>
-          <item.icon />
-          <span>{item.title}</span>
-        </Menu.Item>
-      );
-    });
-  };
+  // const MenuItem = menu => {
+  //   return menu.map(item => {
+  //     if (item.children && item.children.length > 0) {
+  //       return (
+  //         <SubMenu
+  //           key={item.path}
+  //           title={
+  //             <span>
+  //               <item.icon />
+  //               <span>{item.title}</span>
+  //             </span>
+  //           }
+  //         >
+  //           {MenuItem(item.children)}
+  //         </SubMenu>
+  //       );
+  //     }
+  //     return (
+  //       <Menu.Item key={item.path}>
+  //         <item.icon />
+  //         <span>{item.title}</span>
+  //       </Menu.Item>
+  //     );
+  //   });
+  // };
 
   return (
     <div className={styles.Components}>
       <header className={styles.head}>日报生成小工具</header>
       <Layout style={{ minHeight: 'calc(100% - 50px)', background: '#0c1635' }}>
-        <Sider className={styles.sider} trigger={null} collapsible collapsed={collapsed}>
+        {/* <Sider className={styles.sider} trigger={null} collapsible collapsed={collapsed}>
           <Header style={{ padding: 0, textAlign: 'center' }}>
             <MenuFoldOutlined
               className={styles.trigger}
@@ -81,7 +81,7 @@ const Components = (): JSX.Element => {
           >
             {MenuItem(mainRoutes)}
           </Menu>
-        </Sider>
+        </Sider> */}
         <Layout style={{ background: '#0c1635' }}>
           {/* <Breadcrumb style={{ margin: '16px 0' }}>
             <Breadcrumb.Item>User</Breadcrumb.Item>
